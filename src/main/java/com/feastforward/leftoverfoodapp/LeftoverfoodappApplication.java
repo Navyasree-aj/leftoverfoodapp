3a5629e4ac5aae316b4544331173478719d479c2
Navyasree <navyabhavya0512@gmail.com>
2025-09-18 21:17:04 +0530

    Contents of repository changed

---
+++ src/main/java/com/feastforward/leftoverfoodapp/LeftoverfoodappApplication.java
@@ -0,0 +1,13 @@
+package com.feastforward.leftoverfoodapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeftoverfoodappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeftoverfoodappApplication.class, args);
+	}
+
+}
