b73f189c595e34f2e09a58c50f4a77de87657b6d
Navyasree <navyabhavya0512@gmail.com>
2025-10-14 21:38:01 +0530

    Authentication Module

--- src/main/java/com/feastforward/leftoverfoodapp/config/SecurityConfig.java
+++ src/main/java/com/feastforward/leftoverfoodapp/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.feastforward.leftoverfoodapp;
+package com.feastforward.leftoverfoodapp.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
